40b879275ec43a285b8051c0507f2a2d47a02a98
Travis CI User <travis@example.org>
2018-07-23 03:58:08 +0000

    Documentation for Aatch/ramp@6d45c30

---
+++ aliases.js
@@ -0,0 +1,10 @@
+var ALIASES = {};
+ALIASES['hamming'] = {};
+ALIASES['ieee754'] = {};
+ALIASES['libc'] = {};
+ALIASES['num_integer'] = {};
+ALIASES['num_traits'] = {};
+ALIASES['num_traits'] = {};
+ALIASES['ramp'] = {};
+ALIASES['rand'] = {};
+ALIASES['rand_core'] = {};
